df3c086fb63e95d2001813d35a9cde3e1d958025
dtfinch <david@mytsoftware.com>
2017-12-05 20:10:24 -0800

    Update hero.js

--- hero.js
+++ hero.js
@@ -1,26 +1,39 @@
 
-
-//var genes = [1.01,1.12,0.86,1.18,1.53,1,1,1];
-//var genes = [1.07,1.41,1.22,1.43,2.40,1,0.67,0.83];
-//var genes = [1.01, 1.32, 1.16, 1.58, 2.70, 1.00, 0.47, 0.79];
-var genes = [1.04, 1.30, 1.12, 1.53, 2.63, 1, 0.48, 0.81];
+var genes = [0.91, 1.06, 1.00, 1.47, 2.77, 1.00, 0.51, 0.79, 0.83, 1.21, 0.68, 0.65, 0.069, 0.50];
 /*
-for(var i=0;i<8;i++) {
-	genes[i] = Math.round(genes[i]*(0.9+0.2*Math.random())*100)/100;
-}
-console.log("genes = ["+genes.join(", ")+"];")*/
+do {
+	for(var i=0;i<genes.length;i++) {
+		var w = 0.1;
+		var a = 1-w/2;
+		genes[i] = Math.round(genes[i]*(a+w*Math.random())*100)/100;
+	}
+} while(Math.random()<0.5);
+
+console.log("genes "+genes.join(", "));
+*/
+
+
 
 var gameData, helpers;
 var myHero;
+var myTurns = 0;
 
-function getTeamScore(heroes, hero) {
+function getTeamScore(game, hero) {
+	var heroes = game.heroes;
+	var board = game.board;
 	var score=0;
 	for(var i=0; i<heroes.length; i++) {
 		var h = heroes[i];
 		if(!h.dead) {
-			var s = h.health + genes[6]*50 + genes[7]*30*h.mineCount;
-			if(h.id===hero.id) s+=200; //value our life above others
-			//if(isNaN(s)) throw "wtf score " + JSON.stringify(h);
+			var s = h.health;
+			if(h.id===hero.id) {
+				s += s*genes[12] + 200; //value our life above others
+				if(helpers.getAdjacentFilter(h, board, helpers.isWell).length>0) {
+					s += Math.min(100-h.health, 30) * genes[13];
+				}
+				
+			}
+			s += genes[6]*50 + genes[7]*30*h.mineCount
 			
 			score += s * ((h.team===hero.team)?1:-1);
 		}
@@ -33,6 +46,8 @@ var dossier;
 //tries to classify a hero and guess their next move
 function mimic(hero, board) {
 	var d = dossier[hero.id];
+	
+	if(hero.distanceFromLeft!=d.startX || hero.distanceFromTop!=d.startY) d.moved = true;
 
 	var miner = hero.diamondsEarned>0;
 	var healer = hero.healthGiven>0;
@@ -40,7 +55,7 @@ function mimic(hero, board) {
 	var fighter = d.fighter;
 	var target;
 	
-	if(!healer && !miner && !fighter) {
+	if(!healer && !miner && !fighter && (myTurns<=2 || d.moved) && myTurns<=10) {
 		//aggressor is so common, we want to just assume that until we observe better
 		fighter = true;
 	}
@@ -67,18 +82,18 @@ function mimic(hero, board) {
 			
 			if(well && hero.health<((well.distance===1)?100:50)) {
 				target = well;
-			} else if(enemy) {
+			} else {
 				if(healer) {
 					//may heal adjacent allies
 					var ally = helpers.findFirst(hero, board, helpers.isAlly);
 					if(ally && ally.health<=60) target = ally;
 				}
-				if(miner) {
+				if(miner && !target) {
 					//may chase mine if full health
 					if(hero.health===100) target = mine;
 				}
 				
-				if(!target || enemy.distance<target.distance) {
+				if(enemy && (!target || enemy.distance<target.distance)) {
 					var weakerEnemy = helpers.findFirst(hero, board, helpers.isWeakerEnemy);
 					if(weakerEnemy && weakerEnemy.distance<=enemy.distance) {
 						target = weakerEnemy;
@@ -109,11 +124,10 @@ function mimic(hero, board) {
 			} else {
 				target = mine;
 			}
-		} else { //dead code for now
+		} else if(d.moved) {
 			d.type = "survivor";
-			if(well) 
-				target = well;
-		}
+			if(well) target = well;
+		} //else they're probably broken
 	}
 
 	
@@ -122,8 +136,6 @@ function mimic(hero, board) {
 }
 
 var predictions = {};
-/*var lastHeroes;
-var lastPrediction;*/
 var allHits=0, allMisses=0;
 
 function evaluate(direction) {
@@ -142,7 +154,7 @@ function evaluate(direction) {
 	
 	predictions[direction] = game.heroes; 
 	
-	return getTeamScore(game.heroes, myHero);
+	return getTeamScore(game, myHero);
 }
 
 var lastMove="Stay";
@@ -158,7 +170,6 @@ function evalMoves() {
 	for(var dir in helpers.Directions) {
 		if(helpers.validMove(gameData.board, myHero, dir)) {
 			scores[dir] = evaluate(dir);
-			//if(isNaN(scores[dir])) throw "wtf evaluate";
 			if(dir==="Stay") scores[dir]-=5*genes[0]; // bias against staying in one place
 		}
 	}
@@ -172,62 +183,56 @@ function evalMoves() {
 			helpers.isOtherMine(tile, myHero) || helpers.isWell(tile);
 	});
 	var diamondBonus = genes[3]*(0.25+gameData.turn/800);
+	var best, bestValue;
 	for(var i=0; i<targets.length; i++) {
 		var target = targets[i];
 		var tile = target.tile;
 		var value = 0;
 		if(helpers.isAlly(tile)) {
 			value = genes[1]*(100-tile.health);
+			if(tile.healthGiven>0) value += genes[11]*(100-myHero.health); //treat healers like extra semi-wells. made almost no difference in testing :-/
 		} else if(helpers.isEnemy(tile)) {
 			value = genes[2]*Math.max(30+myHero.health-tile.health, 0);
 		} else if(helpers.isOtherMine(tile, myHero)) {
 			value = diamondBonus*Math.max(myHero.health-20, 0);
 		} else if(helpers.isWell(tile)) {
 			value = genes[4]*(100-myHero.health);
-		} else {
-			//throw "wtf "+JSON.stringify(target);
 		}
-		//if(!helpers.Directions[target.dir]) throw "wtf6 "+JSON.stringify(target);
-		//if(!target.distance) throw "wtf distance";
-		//if(isNaN(value)) throw "wtf value";
 		
 		if(scores.hasOwnProperty(target.dir)) { // skip if validMove() identified the direction as pointless
-			scores[target.dir] += genes[5]*0.1*value/target.distance;
+			value /= 1+(target.distance-1)*genes[9];
+			scores[target.dir] += genes[5]*0.1*value;
+			if(!best || value>bestValue) {
+				best = target; bestValue = value;
+			}
 		}
+	}
+	if(best) {
+		scores[best.dir] += genes[10]; 
 	}
 	
 	var avoid = helpers.Inverse[lastMove];
-	if(scores.hasOwnProperty(avoid)) scores[avoid]-=5*genes[0];  // bias against reversing our last turn
+	if(scores.hasOwnProperty(avoid)) scores[avoid]-=5*genes[8];  // bias against reversing our last turn
 	
 	
 	return scores;
 }
 
 
-
-
-
-function initDossier() {
+function newGame() {
+	//forget/reset things that we're not supposed to remember between games
+	//so that it doesn't skew hero-crucible testing
+	
 	dossier = [];
 	var heroes = gameData.heroes;
 	for(var i=0; i<heroes.length; i++) {
-		dossier[heroes[i].id] = {
-			/*hit:0, miss:0,*/ type:"", fighter: false
+		var h = heroes[i];
+		dossier[h.id] = {
+			type:"", fighter: false,
+			startX: h.distanceFromLeft, startY: h.distanceFromTop
 		};
 	}
-}
-
-function newGame() {
-	//forget things that we're not supposed to remember between games
-	//so that it doesn't skew hero-crucible testing
-	//console.log("New Game");
-	/*lastPrediction = undefined;
-	lastHeroes = undefined;*/
-	for(var i in dossier) {
-		var d = dossier[i];
-		d.type = "";
-		d.fighter = "";
-	}
+	myTurns = 0;
 }
 
 var lastTurn = 0;
@@ -236,46 +241,25 @@ function move(g, h) {
 	(helpers = h).setGameData(gameData = g); // :p
 	myHero = g.activeHero;
 	
-	if(!dossier) initDossier();
+	if(!dossier || gameData.turn<lastTurn) newGame();
 	
-	if(gameData.turn<lastTurn) newGame();
-	
-	/*if(lastPrediction) {
-		helpers.testPrediction(lastPrediction, gameData.heroes, dossier);
-	}*/
+	myTurns++;
 	
 	var scores = evalMoves();
 	var best;
 	for(var n in scores) {
-		//if(isNaN(scores[n])) throw "wtf4";
-		//if(n==="undefined") throw "wtf5";
 		if(!best || scores[n]>scores[best]) best=n;
 	}
 	
-	//if(!best) throw "wtf2";
 	
-	//console.log(best + ": " + scores[best]);
-	
-	lastMove = best;
-	/*lastPrediction = predictions[best];
-	lastHeroes = helpers.clone(gameData.heroes);
-	predictions = {}; //free. kinda pointless*/
+	//remember non-passing moves as "Stay" to avoid getting stuck in dead ends (we resist going backwards)
+	lastMove = helpers.passable(helpers.getMoveTile(gameData.board, myHero, best))?"Stay":best;
 	
 	lastTurn = gameData.turn;
 	
 	
-	//helpers.narrate(myHero, gameData.board, best);
-	
 	return best;
 }
-
-
-
-
-
-
-
-
 
 
 module.exports = move;
